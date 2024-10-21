58bda63c24edf46b99f228bf743a906d17f70e7b
Hiya Mavani <108310052+hpmavani@users.noreply.github.com>
2024-10-21 19:51:37 -0400

    Update expressclient.js

--- Supabase_Auth_Demo_Git/expressclient.js
+++ Supabase_Auth_Demo_Git/expressclient.js
@@ -4,13 +4,15 @@ const port = 3000
 const app = express() 
 const path = require('path');
 
+//body parser, necessary for using req.body to get parameters
 app.use(express.urlencoded({ extended: true })); 
 
 //require supabase functions (not defined/implemented yet)
 const {logout, login, getAllBooksAndAuthor} = require('./supabase')
 
+//home page 
 app.get('/', (req, res) => {
-  res.send('welcome to my app')
+  res.send('welcome to my app!')
   logout().then((result) => {
     console.log('Logged out!')
   })
